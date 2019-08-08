09ea6a2eb7cf41ca1e1e28dfdf5f0d80498be41f
AbhayaV2001 <53890164+AbhayaV2001@users.noreply.github.com>
2019-08-08 22:00:45 +0530

    Add files via upload

---
+++ DS2.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+struct student
+{
+	int id;
+	int age,marks;
+};
+void main()
+{
+	struct student st[10];
+	int i;
+	for(i=0;i<10;i++)
+	{
+		printf("enter id,age,marks od student %d \n",i++);
+		scanf("%d %d %d",&st[i].id,&st[i].age,&st[i].marks);
+		if(st[i].age>20 && st[i].marks>=0 && st[i].marks<=100)
+		{
+			printf("data is valid");
+			if(st[i].marks>=65)
+			printf("\n student %d is eligible\n",i);
+			else
+			printf("\n student %d is not eligible\n",i);
+		}
+		else
+		printf("data is invalid\n");
+	}
+	getch();
+}
